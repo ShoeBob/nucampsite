839b4b24523bacf12c60c5a2d4993a5a53f19ac7
Steven Schumacher <shoedds@hotmail.com>
2021-07-24 11:28:23 -0600

    React Native Workshop Task 1

---
+++ components/ContactComponent.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { ScrollView } from "react-native";
+
+class About extends Component {
+
+    static navigationOptions = {
+        title: 'Contact Us'
+    }
+
+    render() {
+        return <ScrollView></ScrollView>;
+    }
+}
+
+export default About;
