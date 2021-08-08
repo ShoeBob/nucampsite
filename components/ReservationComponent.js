35f62abe3def40a6550672c19455f69b679694e6
Steven Schumacher <shoedds@hotmail.com>
2021-08-08 16:02:57 -0600

    Local Notifications

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -3,6 +3,7 @@ import { Text, View, ScrollView, StyleSheet,
     Picker, Switch, Button, Alert } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import * as Animatable from 'react-native-animatable';
+import * as Notifications from 'expo-notifications';
 
 
 class Reservation extends Component {
@@ -23,10 +24,6 @@ class Reservation extends Component {
         title: 'Reserve Campsite'
     }
 
-    // toggleModal() {
-    //     this.setState({showModal: !this.state.showModal});
-    // }
-
     handleReservation() {
         console.log(JSON.stringify(this.state));
         // this.toggleModal();
@@ -46,13 +43,15 @@ class Reservation extends Component {
                 },
                 {
                     text: 'OK',
-                    onPress: () => this.resetForm()
+                    onPress: () => {
+                        this.presentLocalNotification(this.state.date.toLocaleDateString('en-US'))
+                        this.resetForm()
+                    }
                 }
             ],
             {cancelable: false}
         );
     }
-
 
     resetForm() {
         this.setState({
@@ -60,8 +59,33 @@ class Reservation extends Component {
             hikeIn: false,
             date: new Date(),
             showCalendar: false,
-            // showModal: false
         });
+    }
+
+    async presentLocalNotification(date) {
+        function sendNotification() {
+            Notifications.setNotificationHandler({
+                handleNotification: async () => ({
+                    shouldShowAlert: true
+                })
+            });
+
+            Notifications.scheduleNotificationAsync({
+                content: {
+                    title: 'Your Campsite Reservation Search',
+                    body: `Search for ${date} requested`
+                },
+                trigger: null
+            });
+        }
+
+        let permissions = await Notifications.getPermissionsAsync();
+        if (!permissions.granted) {
+            permissions = await Notifications.requestPermissionsAsync();
+        }
+        if (permissions.granted) {
+            sendNotification();
+        }
     }
 
     render() {
@@ -122,33 +146,6 @@ class Reservation extends Component {
                             accessibilityLabel='Tap me to search for available campsites to reserve'
                         />
                     </View>
-                    {/* <Modal
-                        animationType={'slide'}
-                        transparent={false}
-                        visible={this.state.showModal}
-                        onRequestClose={() => this.toggleModal()}
-                    >
-                        <View style={styles.modal}>
-                            <Text style={styles.modalTitle}>Search Campsite Reservations</Text>
-                            <Text style={styles.modalText}>
-                                Number of Campers: {this.state.campers}
-                            </Text>
-                            <Text style={styles.modalText}>
-                                Hike-In?: {this.state.hikeIn ? 'Yes' : 'No'}
-                            </Text>
-                            <Text style={styles.modalText}>
-                                Date: {this.state.date.toLocaleDateString('en-US')}
-                            </Text>
-                            <Button
-                                onPress={() => {
-                                    this.toggleModal();
-                                    this.resetForm();
-                                }}
-                                color='#5637DD'
-                                title='Close'
-                            />
-                        </View>
-                    </Modal> */}
                 </Animatable.View>
             </ScrollView>
         );
@@ -170,22 +167,6 @@ const styles = StyleSheet.create({
     formItem: {
         flex: 1
     },
-    // modal: { 
-    //     justifyContent: 'center',
-    //     margin: 20
-    // },
-    // modalTitle: {
-    //     fontSize: 24,
-    //     fontWeight: 'bold',
-    //     backgroundColor: '#5637DD',
-    //     textAlign: 'center',
-    //     color: '#fff',
-    //     marginBottom: 20
-    // },
-    // modalText: {
-    //     fontSize: 18,
-    //     margin: 10
-    // }
 });
 
 export default Reservation;
